3b98bed3f3fafad5cd5502a8d1d982472f7154a7
stuart lamble <lamble.stuart@googlemail.com>
2019-11-22 21:37:28 +0000

    created question arrays in questions.js

--- Questions.js
+++ Questions.js
@@ -6,3 +6,31 @@
     //     choices: ["strings", "booleans", "alerts", "numbers"],
     //     answer: "alerts"
     //     }
+
+        var questions = [
+        {
+            title: "What two values does the boolean data type accept?",
+            choices: ["up & down", "left & right", "true & false", "light & dark"],
+            answer: "true & false"
+        },
+        {
+            title: "Where var x = 50 and var y = 100 what does console.log(x+y) show?",
+            choices: ["50 100", "150", "50000", "50 + 100"],
+            answer: "150"
+        },
+        {
+            title: "For loops consist of how many components?",
+            choices: ["1", "2", "3", "4"],
+            answer: "3"
+        },
+        {
+            title: "What is a function?",
+            choices: ["A block of code that can be reused", "a matrix of numbers", "a list of strings", "a job"],
+            answer: "A block of code that can be reused"
+        },
+        {
+            title: "Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+        }
+        ]
